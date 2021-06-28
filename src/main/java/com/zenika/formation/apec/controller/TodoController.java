9bff0d9b33411ca64d8a14518cc2b9559ce45fbf
Lokymil <arrestierchristophe@gmail.com>
2021-06-28 10:03:35 +0200

    Prepare basic route to be used

--- src/main/java/com/zenika/formation/apec/controller/TodoController.java
+++ src/main/java/com/zenika/formation/apec/controller/TodoController.java
@@ -3,8 +3,7 @@ package com.zenika.formation.apec.controller;
 import com.zenika.formation.apec.model.Todo;
 import com.zenika.formation.apec.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,7 +13,22 @@ public class TodoController {
     private TodoService todoService;
 
     @GetMapping("/todos")
-    public List<Todo> list() {
+    public List<Todo> listAll() {
         return todoService.getAllTodos();
+    }
+
+    @PostMapping("/todos")
+    public String createOne() {
+        return "Not implemented yet";
+    }
+
+    @GetMapping("/todos/{id}")
+    public String getOne(@PathVariable("id") int id) {
+        return "Not implemented yet for id : " + id;
+    }
+
+    @PutMapping("/todos/{id]")
+    public String updateOne(@PathVariable("id") int id) {
+        return "Not implemented yet for id : " + id;
     }
 }
